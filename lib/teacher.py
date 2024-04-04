c89815e2ffd5d20e978353af03f1d69d9f44fc1d
PapiLuee <41023873+LueDev@users.noreply.github.com>
2024-04-04 14:04:24 -0400

    Teacher and student inheritance is pretty cool and useful. All tests passing

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,10 @@ from user import User
 import random
 
 class Teacher(User):
+    
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Subjects"]
 
     def teach(self):
-        pass
+        return "Subjects"
